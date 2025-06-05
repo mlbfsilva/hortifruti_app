e7e651a3e6e03d302ab8149d47c09f119879a8d8
Maria Luiza <marialuiza.bf.silva@gmail.com>
2025-06-04 21:39:29 -0300

    dashboard parceiros

---
+++ app/parceiros/(tabs)/dashboard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+
+export default function DashboardParceiro() {
+  return (
+    <Text> hello </Text>
+  );}
