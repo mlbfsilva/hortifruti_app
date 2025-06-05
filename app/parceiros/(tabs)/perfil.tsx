753cc47789e78a4576519e8e467ab56eb4d4a01f
Maria Luiza <marialuiza.bf.silva@gmail.com>
2025-06-04 21:38:47 -0300

    perfil parceiros

---
+++ app/parceiros/(tabs)/perfil.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+
+export default function PerfilParceiro() {
+  return (
+    <Text> hello </Text>
+  );}
